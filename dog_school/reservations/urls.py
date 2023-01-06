680a06fb4ce0f9ee0f21cfc8b32afb950017b3d8
Jan Travnicek <trawais002@gmail.com>
2023-01-06 13:36:00 +0100

    add app_name

--- dog_school/reservations/urls.py
+++ dog_school/reservations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'course'
 urlpatterns = [
-    path('courses/', views.courses, name='courses'),
-    path('courses/<int:pk>/', views.course_detail, name='course_detail'),
+    path('courses/', views.courses, name='all'),
+    path('course/<int:pk>/', views.course_detail, name='detail'),
 ]
