67b9bf2a68b763bd8b98dacf2c7c9ac339776d19
Jan Travnicek <trawais002@gmail.com>
2023-01-17 21:00:54 +0100

    clean lessons older than 7 days

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'reservations'
 urlpatterns = [
-    path('courses/', views.courses, name='all'),
+    path('', views.courses, name='all'),
     path('course/<int:pk>/', views.course_detail, name='course'),
     path('course/lesson/<int:pk>/', views.lesson_detail, name='lesson'),
 ]
