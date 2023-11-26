f163d813cd86a5b02bb08c4de906a2da4bd6e000
jan travnicek <jan.travnicek@bloomreach.com>
2023-11-26 09:40:04 +0100

    prettify env vars logging

--- vercel-build_files.sh
+++ vercel-build_files.sh
@@ -2,13 +2,13 @@ python3.9 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 
-echo $VIRTUAL_ENV
+echo "Python venv path: ${VIRTUAL_ENV}"
 
-echo $POSTGRES_DATABASE
-echo $POSTGRES_USER
-echo $POSTGRES_PASSWORD
-echo $POSTGRES_HOST
-echo $POSTGRES_PORT
+echo "Postgres database: ${POSTGRES_DATABASE}"
+echo "Postgres user: ${POSTGRES_USER}"
+echo "Postgres password: ${POSTGRES_PASSWORD}"
+echo "Postgres host: ${POSTGRES_HOST}"
+echo "Postgres port: ${POSTGRES_PORT}"
 
 pip install -r requirements.txt
 
